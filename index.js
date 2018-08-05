4060fd467b6ae550c59aec341454e597ddaed86d
Alain Lafume <Lafume.alain@gmail.com>
2018-08-05 14:38:09 -0400

    third commit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Include the server in your file
 const server = require('server');
+
+const app = require(express);
 const { get, post } = server.router;
 
 // Handle requests to the url "/" ( http://localhost:3000/ )
-server([
-  get('/', ctx => 'Hello world!')
-]);
+// server([
+//   get('/', ctx => 'Hello world!')
+// ]);
+
+app.get('/', function(req,res) {
+    res.sendfile('./index.html');
+   });
