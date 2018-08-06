f7f50a19be7fe922a584145d5cf0b01f98177ebf
Alain Lafume <Lafume.alain@gmail.com>
2018-08-06 16:36:05 -0400

    another commit fixes

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://savage:savage1@ds127781.mlab.com:27781/21savage'  // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
